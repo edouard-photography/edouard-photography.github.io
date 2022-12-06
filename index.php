f2fa73a4979935dfa130e4ebaf686feac5842776
edouard-photography <119431428+edouard-photography@users.noreply.github.com>
2022-12-06 16:59:54 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+		<!-- BEGIN head.php INCLUDE -->
+		<?php include "./elements/head.php"; ?>
+		<!-- END head.php INCLUDE -->
+    </head>
+   
+	<body>
+		<div class="grid-wrapper">
+			<header>
+			</header>
+			<nav>
+				<!-- BEGIN navbar.php INCLUDE -->
+				<?php include "./elements/navbar.php"; ?>
+				<!-- END navbar.php INCLUDE -->
+			</nav>
+			<article>
+				<h3>Plateau de Lautagne</h3>
+				<p class="intro">
+				</p>
+				<div class="row">
+					<div class="longue">
+						<a href="#img1">
+							<img src="photos/plateau_de_lautagne/thumbnails/1.webp" alt="petite légende">
+						</a>
+						<a href="#" class="lightbox" id="img1">
+							<span style="background-image: url('photos/plateau_de_lautagne/1.webp')"></span>
+							<p>petite légende</p>
+						</a>
+					</div>
+					<div class="longue">
+						<a href="#img2">
+							<img src="photos/plateau_de_lautagne/thumbnails/2.webp" alt="petite légende">
+						</a>
+						<a href="#" class="lightbox" id="img2">
+							<span style="background-image: url('photos/plateau_de_lautagne/2.webp')"></span>
+							<p>petite légende</p>
+						</a>
+					</div>
+					<div class="carre">
+						<a href="#img3" 
+						target="_blank" rel="noopener">
+						<img src="photos/plateau_de_lautagne/thumbnails/3.webp" alt="">
+						</a>
+						<a href="#" class="lightbox" id="img3">
+							<span style="background-image: url('photos/plateau_de_lautagne/3.webp')"></span>
+							<p>petite légende</p>
+						</a>
+					</div>
+				</div>
+				<div class="row">
+					<div class="longue">
+						<a href="#img4">
+							<img src="photos/plateau_de_lautagne/thumbnails/4.webp" alt="petite légende">
+						</a>
+						<a href="#" class="lightbox" id="img4">
+							<span style="background-image: url('photos/plateau_de_lautagne/4.webp')"></span>
+							<p>petite légende</p>
+						</a>
+					</div>
+					<div class="longue">
+						<a href="#img5">
+							<img src="photos/plateau_de_lautagne/thumbnails/5.webp" alt="petite légende">
+						</a>
+						<a href="#" class="lightbox" id="img5">
+							<span style="background-image: url('photos/plateau_de_lautagne/5.webp')"></span>
+							<p>petite légende</p>
+						</a>
+					</div>						
+					<div class="photo">
+						<a href="#img6">
+							<img src="photos/plateau_de_lautagne/thumbnails/6.webp" alt="petite légende">
+						</a>
+						<a href="#" class="lightbox" id="img6">
+							<span style="background-image: url('photos/plateau_de_lautagne/6.webp')"></span>
+							<p>petite légende</p>
+						</a>
+					</div>
+				</div>
+				<div class="row">
+					<div class="longue">
+						<a href="#img7">
+							<img src="photos/plateau_de_lautagne/thumbnails/7.webp" alt="petite légende">
+						</a>
+						<a href="#" class="lightbox" id="img7">
+							<span style="background-image: url('photos/plateau_de_lautagne/7.webp')"></span>
+							<p>petite légende</p>
+						</a>
+					</div>
+					<div class="photo">
+						<a href="#img8">
+							<img src="photos/plateau_de_lautagne/thumbnails/8.webp" alt="petite légende">
+						</a>
+						<a href="#" class="lightbox" id="img8">
+							<span style="background-image: url('photos/plateau_de_lautagne/8.webp')"></span>
+							<p>petite légende</p>
+						</a>
+					</div>
+					<div class="photo">
+						<a href="#img9">
+							<img src="photos/plateau_de_lautagne/thumbnails/9.webp" alt="petite légende">
+						</a>
+						<a href="#" class="lightbox" id="img9">
+							<span style="background-image: url('photos/plateau_de_lautagne/9.webp')"></span>
+							<p>petite légende</p>
+						</a>
+					</div>					
+				</div>
+				<div class="row">
+					<div class="photo">
+						<a href="#img10">
+							<img src="photos/plateau_de_lautagne/thumbnails/10.webp" alt="petite légende">
+						</a>
+						<a href="#" class="lightbox" id="img10">
+							<span style="background-image: url('photos/plateau_de_lautagne/10.webp')"></span>
+							<p>petite légende</p>
+						</a>
+					</div>
+					<div class="photo">
+						<a href="#img11">
+							<img src="photos/plateau_de_lautagne/thumbnails/11.webp" alt="petite légende">
+						</a>
+						<a href="#" class="lightbox" id="img11">
+							<span style="background-image: url('photos/plateau_de_lautagne/11.webp')"></span>
+							<p>petite légende</p>
+						</a>
+					</div>
+					<div class="photo">
+						<a href="#img12">
+							<img src="photos/plateau_de_lautagne/thumbnails/12.webp" alt="petite légende">
+						</a>
+						<a href="#" class="lightbox" id="img12">
+							<span style="background-image: url('photos/plateau_de_lautagne/12.webp')"></span>
+							<p>petite légende</p>
+						</a>
+					</div>					
+				</div>
+				<div class="row">
+					<div class="photo">
+						<a href="#img13">
+							<img src="photos/plateau_de_lautagne/thumbnails/13.webp" alt="petite légende">
+						</a>
+						<a href="#" class="lightbox" id="img13">
+							<span style="background-image: url('photos/plateau_de_lautagne/13.webp')"></span>
+							<p>petite légende</p>
+						</a>
+					</div>
+					<div class="photo">
+						<a href="#img14">
+							<img src="photos/plateau_de_lautagne/thumbnails/14.webp" alt="petite légende">
+						</a>
+						<a href="#" class="lightbox" id="img14">
+							<span style="background-image: url('photos/plateau_de_lautagne/14.webp')"></span>
+							<p>petite légende</p>
+						</a>
+					</div>
+					<div class="photo">
+						<a href="#img15">
+							<img src="photos/plateau_de_lautagne/thumbnails/15.webp" alt="petite légende">
+						</a>
+						<a href="#" class="lightbox" id="img15">
+							<span style="background-image: url('photos/plateau_de_lautagne/15.webp')"></span>
+							<p>petite légende</p>
+						</a>
+					</div>					
+				</div>
+			</article>
+			<footer>
+			   	<!-- BEGIN footer.php INCLUDE -->
+				<?php include "./elements/footer.php"; ?>
+				<!-- END footer.php INCLUDE -->
+			</footer>
+		</div>
+    </body>
+</html>
